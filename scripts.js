ae0238783a54109a98c3f054457f8020f5c781f2
Kato <ferreyradamian3755@gmail.com>
2024-03-26 12:46:23 -0300

    Data

--- scripts.js
+++ scripts.js
@@ -11,5 +11,25 @@ function copyText(htmlElement) {
     inputElement.parentNode.removeChild(inputElement);
 }
 document.querySelector("#Mail").onclick = function() {
+    let mail="ferreyradamian3755@gmail.com"
     copyText(document.querySelector("#Mail"))
+    alert("Correo copiado a tu portapapeles")
 }            
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("myHeader");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
