902520e9c4df219dbeaba38f25e83c222723377a
Kato <ferreyradamian3755@gmail.com>
2024-04-06 01:24:16 -0300

    si
    dark theme y audios

--- scripts.js
+++ scripts.js
@@ -38,14 +38,35 @@ const checkbox = document.getElementById("checkbox");
 
 checkbox.addEventListener("change", () => {
     document.body.classList.toggle("dark");
-    var sections = document.getElementsByClassName('section');
-    var seclen = sections.length;
-    console.log(seclen);
-    console.log(sections[0]);
-    console.log(sections[1]);
-    console.log(sections[2]);
-    for(var i = 0; i <= seclen-1; i++) {
-        sections[i].className = "sectiongray";
-        console.log(i)
+    let footer = document.getElementsByClassName("footer");
+    let footerg = document.getElementsByClassName("footerg");
+    let header = document.getElementsByClassName("header");
+    let headerg = document.getElementsByClassName("headerg");
+    let sections = document.getElementsByClassName("section");
+    let sectionsg = document.getElementsByClassName("sectiongray");
+    if (sections.length > 0) {
+        for (let i=0; sections.length > i; i) {
+            sections[i].className = "sectiongray";
+            console.log(i);
+        }
+        footer[0].className = "footerg";
+        header[0].className = "headerg";
+    } else {
+        for (let i=0; sectionsg.length > i; i) {
+            sectionsg[i].className = "section";
+            console.log(i);
+        }
+        footerg[0].className = "footer";
+        headerg[0].className = "header";
     }
 });
+
+function clear_click() {
+    var audio = document.getElementById("clear_click");
+    audio.play();
+}
+
+function tech_click() {
+    var audio = document.getElementById("tech_click");
+    audio.play();
+}
